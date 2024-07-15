20d32a4bc14602545d7f98fde56059ad7f532831
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-07-15 19:28:22 -0400

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AuthLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className='grid place-items-center min-h-screen'>{children}</div>
+    )
+}
+
+export default AuthLayout
---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import FormLogin from '@/components/Form-login'
+
+const LoginPage = () => {
+  return (
+    <div>LoginPage
+
+      <FormLogin/>
+    </div>
+  )
+}
+
+export default LoginPage
